655e575508b10b7db06506c59fec309371158b7a
Portfedh <59422723+portfedh@users.noreply.github.com>
2021-11-30 17:47:26 -0600

    First Commit

---
+++ caesar_cypher.py
@@ -0,0 +1,29 @@
+# Caesar Cypher.
+# Must use capital letters.
+
+
+def generate_key(n):
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = {}
+    cnt = 0
+    for c in letters:
+        key[c] = letters[(cnt + n) % len(letters)]
+        cnt += 1
+    return key
+
+
+def encrypt(key, message):
+    cipher = ""
+    for c in message:
+        if c in key:
+            cipher += key[c]
+        else:
+            cipher += c
+    return cipher
+
+
+key = generate_key(3)
+message = "Test Message"
+cipher = encrypt(key, message)
+print(key)
+print(cipher)
